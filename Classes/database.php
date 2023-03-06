52e7f596d663136a767dd54e57df22efdd8ccf00
John Renzo M. Crisostomo <johnrenzo.crisostomo07@gmail.com>
2023-03-06 23:54:51 +0800

    cloud

--- Classes/database.php
+++ Classes/database.php
@@ -2,13 +2,13 @@
 
 class Database
 {
-    private $dbServername = "localhost";
-    private $dbUsername = "root";
-    private $dbPassword = "";
+    private $dbServername = "sql985.main-hosting.eu";
+    private $dbUsername = "u839345553_sbit3g";
+    private $dbPassword = "sbit3gQCU";
 
     public function connect()
     {
-        $connect = new PDO("mysql:host=$this->dbServername;dbname=sbit3g", $this->dbUsername, $this->dbPassword);
+        $connect = new PDO("mysql:host=$this->dbServername;dbname=u839345553_SBIT3G", $this->dbUsername, $this->dbPassword);
 
         // set the PDO error mode to exception
         $connect->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
