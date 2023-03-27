857dc3ef73143cbc38f4b7ae984398f8637ad69f
Shine <parkshayneu@gmail.com>
2023-03-27 21:31:37 +0800

    Admin Attendance List (Frontend)

--- Pages/admin-attendanceList.php
+++ Pages/admin-attendanceList.php
@@ -1,52 +1,64 @@
 <?php
 // start session
 session_start();
-
-
 if (isset($_SESSION['admin_id']) && $_SESSION['admin_id'] == 1) {
     require '../Classes/employee.php';
     require '../Classes/database.php';
 
     $database = new Database();
     $attlist = new Employee($database);
-
-    
 } else {
     header("Location: ../index.php");
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-  <style>
-    table,th,td{
-        border:1px solid black;
-        padding: 10px;
-        margin: 10px;
-    }
-    </style>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://kit.fontawesome.com/53a2b7f096.js" crossorigin="anonymous"></script>
 </head>
 
-<body>
-    <h1>Attendance</h1>
-    <div>
-    <table>
-    <tr>
-        <th>Id</th>
-        <th>Employee Id</th>
+<body style="background-color: #f2f2f2; font-family: Bahnschrift;">
+<!--Time and Date-->
+<div class="container-fluid d-flex justify-content-center align-items-center mt-4">
+    <h5 style="font-weight:bolder;"> 
+      <script>                   
+        document.write(new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true, day: 'numeric', month: 'short', year: 'numeric' }).toUpperCase());
+      </script>    
+    </h5>  
+</div>
+<!------End----->
+
+<div class="container">
+  <h4 class="fw-bolder">Attendance</h4>
+  <button class="btn border-0 border shadow-none border-start-0 mb-1">
+    <h4><i class="fa-solid fa-clock-rotate-left"></i></h4>
+  </button>
+  <div class="col-sm input-group w-25 float-end">
+      <span class="input-group-text bg-white border border-end-0 border-0">
+        <i class="fa-solid fa-magnifying-glass"></i>
+      </span>
+      <input type="text" class="form-control border-0 border shadow-none border-start-0" id="Search" autocomplete="off">
+    </div>
+    
+<!----Table Start--->
+  <table class="table table-striped table-borderless align-middle text-center">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Employee ID</th>
         <th>Date</th>
-        <th>Time_In</th>
+        <th>Time IN</th>
+        <th>Time OUT</th>
         <th>Status</th>
-        <th>Time_out</th>
-    </tr>
-    <?php
-      $attlist = $attlist->attendanceList();
-      foreach($attlist as $list){
-    ?>
-    <tr>
+      </tr>
+      <?php
+        $attlist = $attlist->attendanceList();
+        foreach($attlist as $list){
+      ?>
+      <tr>
       <td><?php echo $list['id']; ?> </td>
       <td><?php echo $list['employee_id']; ?> </td>
       <td><?php echo $list['date']; ?> </td>
@@ -55,19 +67,22 @@ if (isset($_SESSION['admin_id']) && $_SESSION['admin_id'] == 1) {
           $formattedTimeIn = $time->format('h:i:s A'); 
            echo $formattedTimeIn;
             ?>
-      </td>
-      <td><?php echo $list['status']; ?> </td>
+      </td>  
       <td><?php 
             $time = DateTime::createFromFormat('H:i:s', $list['time_out']);
             $formattedTimeOut = $time->format('h:i:s A'); 
             echo $formattedTimeOut;
           ?> 
       </td>
+      <td><?php echo $list['status']; ?> </td>
     </tr>
     <?php
       }
     ?>
-    </table>
-    </div>
+    </thead>
+  </table>
+<!----Table End--->
+</div>
+
 </body>
 </html>
