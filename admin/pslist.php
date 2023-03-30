dd181c351caca4ea75280aee4d718ce8532dbd8a
Shine <parkshayneu@gmail.com>
2023-03-30 19:57:11 +0800

    Login Revamped (Frontend)

--- admin/pslist.php
+++ admin/pslist.php
@@ -19,14 +19,14 @@ if (isset($_SESSION['admin_id']) && $_SESSION['admin_id'] == 1) {
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://kit.fontawesome.com/53a2b7f096.js" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="../includes/prnew.js"></script>
 </head>
-<body>
+<body style="background-color: #f2f2f2; font-family: Bahnschrift;">
 <div class="container">
  <div class="row">
    <div class="col-sm-8">
--- index.php
+++ index.php
@@ -6,94 +6,84 @@ session_start();
 
 if (isset($_SESSION['admin_id']) && $_SESSION['admin_id'] == 1) {
    header("Location: Pages/dashboard.php");
-
 } 
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>HR MANAGEMENT SYSTEM</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-
-<script src="https://kit.fontawesome.com/53a2b7f096.js" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>HR MANAGEMENT SYSTEM</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/53a2b7f096.js" crossorigin="anonymous"></script>
 </head>
-<body>
-
+<body style="background-color: #f2f2f2; font-family: Bahnschrift;">
 
 <div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-6 ">
+            <nav class="navbar bg-body-tertiary">
+                <div class="container">
+                    <a class="navbar-brand color-black" href="#">
+                    <img src="./images/Google-Admin-900x0.png" alt="Admin" width="70" height="50">
+                    ADMIN
+                    </a>
+                </div>
+            </nav>
 
-  <div class="row">
-    <div class="col-sm-6 ">
-	<nav class="navbar bg-body-tertiary">
-  <div class="container">
-    <a class="navbar-brand color-black" href="#">
-      <img src="./images/Google-Admin-900x0.png" alt="Admin" width="70" height="50">
-	  ADMIN
-    </a>
-  </div>
-</nav>
-
-<div class="container m-auto ">
-<h4 class="text-center my-3">Start your day with hello Kitty</h4>
-<img src="./images/pngtree-cartoon-company-employee-who-is-working-in-the-office-image_1377082-removebg-preview.png" class="img-fluid mx-auto d-block">
-</div>
-
-
-	</div>
-
-    <div class="col-sm-6 d-flex justify-content-md-center align-items-center vh-100">
-
-	<div class="container col-9 shadow-lg p-4"> 
-	<div class="login-form ">
-    <form action="Functions/admin-login.php" method="POST">
-        <h4 class="text-center">Company Name</h4>   
-
-        <?php
-       
-                if (isset($_GET["error"])) {
-                    echo '<div class="container rounded shadow text-center p-3 text-danger" style="background-color: #ff9694;">';
-                    if ($_GET["error"] == "errorPassword") {
-                        echo '<h5 class=""> Wrong password!</h5>';
-                    } else if ($_GET["error"] == "errorEmail") {
-                        echo '<p class=""> Email does not exist!!</p>';
-                    }else if ($_GET["error"] == "emptyInput") {
-                        echo '<p class=""> Empty Input</p>';
-                    }
-                    echo '</div>';
-                }
-                
-            ?> 
-        <div class="form-group py-2" >
-			<label for="exampleInputEmail1" class="form-label">Email</label>
-
-			<div class="input-group">
-			<div class="input-group-text bg-transparent border-right-0"><i class="fa-solid fa-user"></i></div>
-        	<input type="text" class="form-control  border-left-0" name="email" placeholder="Email" required="required">
-		</div>
-		</div>
+            <div class="container m-auto ">
+                <h4 class="text-center my-3">Start your day with Renzo Caloocan</h4>
+                <img src="./images/login-illustration.png" class="mx-auto d-block" height="400">
+            </div>
         </div>
 
-		<div class="form-group py-1">
-		<label for="examplePassword" class="form-label">Password</label>
+        <div class="col-sm-6 d-flex justify-content-md-center align-items-center vh-100">
+	        <div class="container col-9 shadow-lg p-4"> 
+	            <div class="login-form ">
+                    <form action="Functions/admin-login.php" method="POST">
+                    <h4 class="text-center">Company Name</h4>   
+                    <?php
+                            if (isset($_GET["error"])) {
+                                echo '<div class="container rounded shadow text-center p-3 text-danger" style="background-color: #ff9694;">';
+                                if ($_GET["error"] == "errorPassword") {
+                                    echo '<h5 class=""> Wrong password!</h5>';
+                                } else if ($_GET["error"] == "errorEmail") {
+                                    echo '<p class=""> Email does not exist!!</p>';
+                                }else if ($_GET["error"] == "emptyInput") {
+                                    echo '<p class=""> Empty Input</p>';
+                                }
+                                echo '</div>';
+                            }            
+                        ?> 
+                    <div class="form-group py-2" >
+			            <label for="exampleInputEmail1" class="form-label fw-bolder">Email</label>
+                            <div class="input-group">
+                                <div class="input-group-text bg-transparent border-right-0"><i class="fa-solid fa-user"></i></div>
+                                    <input type="text" class="form-control shadow-none border-left-0" name="email" placeholder="Email" required="required">
+                                </div>
+                            </div>
+                    </div>
 
-		<div class="input-group">
-			<div class="input-group-text bg-transparent border-right-0"><i class="fa-solid fa-lock"></i></div>
-            <input type="password" class="form-control border-left-0" name="password" placeholder="Password" required="required">
+		    <div class="form-group py-1">
+		        <label for="examplePassword" class="form-label fw-bolder">Password</label>
+
+		        <div class="input-group">
+			        <div class="input-group-text bg-transparent border-right-0"><i class="fa-solid fa-lock"></i></div>
+                        <input type="password" class="form-control shadow-none border-left-0" name="password" placeholder="Password" required="required">
 		
-		</div>
-		</div>        
+		            </div>
+		        </div>        
 
-        <div class="form-group py-2">
-            <button type="submit" class="btn btn-primary btn-md btn-block">Log in</button>
-	</div>
-        </form>
-		</div>
+                    <div class="form-group py-2">
+                    <button type="submit" class="btn btn-primary btn-md btn-block">Log in</button>
+                    </div>
+                </form>
+		    </div>
 	</div>
   </div>
 
