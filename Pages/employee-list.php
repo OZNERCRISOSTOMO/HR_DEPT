036c3abf05c97c66ab44baf61674ede537786285
Elle-lien <rochelle.pabilane30@gmail.com>
2023-04-12 23:14:44 +0800

    employee list update

--- Pages/employee-list.php
+++ Pages/employee-list.php
@@ -60,8 +60,8 @@
     <!-----Employee List----->
     <div class="container mt-5">
     <div class="row-cols-4">
-    <div class="card cols-1 bg-white rounded mx-2 py-3">
-                        <img class="rounded-circle mx-auto" src="../img/Shipoo.jpg" height="100" width="100" alt="Employee Pic">    
+    <div class="card cols-1 bg-white rounded mx-2 py-3" data-bs-toggle="modal" id="view" data-bs-target="#viewmodal">
+                        <img class="rounded-circle mx-auto" src="../Images/1x1 photo.png" height="100" width="100" alt="Employee Pic">    
                         <div class="card-body ps-1">
                             <h4 class="card-title text-center" name="EmployeeName">Shipoo Banini</h4>
                             <p class="card-text text-center" style="opacity: 0.5;">Project Manager</p>
@@ -83,17 +83,6 @@
                             
                             <p class="card-text ms-3" name="Email"><i class="fa-solid fa-envelope text-primary"></i> shipoobanini@gmail.com</p>
                             <p class="card-text ms-3" name="ContactNum"><i class="fa-solid fa-phone text-success"></i> 09123456789</p>
-                                    <div class="float-end align-middle">
-                                    <button type="button" class="btn" data-bs-toggle="modal" id="view" data-bs-target="#viewmodal">
-                                        <i class="fa-solid fa-up-right-and-down-left-from-center text-secondary mx-0"></i>
-                                    </button>
-                                    <button type="button" class="btn" data-bs-toggle="modal" id="edit" data-bs-target="#editmodal">
-                                        <i class="fa-solid fa-pen-to-square text-primary"></i>
-                                    </button>
-                                    <button type="button" class="btn" id="delete">
-                                        <i class="fa-solid fa-square-minus text-danger"></i>
-                                    </button>
-                                    </div>
                         </div>
                         </div>
                     </div>
@@ -103,41 +92,78 @@
 
         <!-- Modal for View -->
         <div class="modal fade" id="viewmodal">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-scrollable modal-lg">
         <div class="modal-content">
 
         <!-- Modal Header -->
         <div class="modal-header">
-            <h5 class="modal-title">View</h5>
+            <h5 class="modal-title">Employee Data</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
         </div>
 
         <!-- Modal body -->
+
         <div class="modal-body">
-            <div class="form-floating">
-            <input type="text" class="form-control" name="EmployeeName" id="floatingName" placeholder="Employee Name"> </textarea>
-            <label for="floatingName" style="font-family: Bahnschrift;">Employee Name</label>
-            </div>
+  <div class="d-flex flex-row">
+    <div class="flex-fill p-2">
+      <img class="rounded-circle mx-auto d-block" src="../Images/1x1 photo.png" height="150" width="150" alt="Employee Pic">
+      <h2 class="text" name="EmployeeName">Renzo Caloocan</h2>
+      <p class="text text-center">Project Manager</p>
+      <div class="rounded ms-3"></div>
+    </div>
+    <div class="flex-fill p-2">
+      <div class="form-floating">
+        <h5 style="font-family: Bahnschrift;">Personal Information</h5>
+        <table class="table table-borderless mt-2">
+          <thead>
+            <tr>
+              <th><p class="text" style="opacity: 0.5;">Employee ID </p></th>
+              <th><p class="text" style="opacity: 0.5;">Date Hired </p></th>
+              <th><p class="text" style="opacity: 0.5;">Email </p></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><p class="text" style=";">0001-AAA</p></td>
+              <td>10/02/2023</td>
+              <td>johnrenzocrisos@gmail.com</td>
+            </tr>
+            <tr>
+              <th><p class="text" style="opacity: 0.5;">Contact Number </p></th>
+              <th><p class="text" style="opacity: 0.5;">Birthdate </p></th>
+            </tr>
+            <tr>
+              <td>+639123456789</td>
+              <td>21/02/2002</td>
+            </tr>
+          </tbody>
+        </table>
+        <h5 style="font-family: Bahnschrift;">Benefits</h5>
+        <table class="table table-borderless mt-2">
+          <thead>
+            <tr>
+              <th><p class="text" style="opacity: 0.5;">SSS </p></th>
+              <th><p class="text" style="opacity: 0.5;">TIN </p></th>
+              <th><p class="text" style="opacity: 0.5;">PhilHealth </p></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><p class="text" style=";">0000-0000000-0</p></td>
+              <td>000-000-000-0000</td>
+              <td>0000-0000-0000</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
 
-        <div class="form-floating mt-3">
-            <textarea class="form-control" name="EmployeePos" id="floatingPosition" placeholder="Employee Position"> </textarea>
-            <label for="floatingPosition" style="font-family: Bahnschrift;">Employee Position</label>
-            </div>
-
-        <div class="dropdown mt-3">
-            <select name="Category" id="category">
-            <option value="Category">Department</option>
-            <option value="Sales">Sales</option>
-            <option value="HR">HR</option>
-            <option value="Secret">Secret</option>
-            </select>
-        </div>
-        </div>
         <!--Modal Body End-->
 
         <!-- Modal footer -->
         <div class="modal-footer">
-            <button type="button" class="btn btn-success" name="add-details">Submit</button>
             <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
         </div>
         </div>
@@ -152,7 +178,7 @@
 
         <!-- Modal Header -->
         <div class="modal-header">
-            <h5 class="modal-title">Edit</h5>
+            <h5 class="modal-title">Employee Data</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
         </div>
 
@@ -188,6 +214,10 @@
     </div>
     </div>
     <!-----End----->
+
+    
+
+
 
 </body>
 </html>
