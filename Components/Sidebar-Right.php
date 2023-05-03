c420f410cb588755da8d3137df374de83b85fb4d
John Renzo M. Crisostomo <johnrenzo.crisostomo07@gmail.com>
2023-05-03 17:57:59 +0800

    updates

--- Components/Sidebar-Right.php
+++ Components/Sidebar-Right.php
@@ -1,5 +1,6 @@
-
+<html>
 <title>Bootstrap demo</title>
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"><style>
 
 .container1 {
@@ -25,16 +26,25 @@
                 
   <div class="w-100">
  <h5 class="text-black mt-2">Holidays</h5>
-  <div class="d-grid gap-2 mb-2 mt-2">
-
-  <button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#Holiday">
-  <i class="fa-solid fa-plus"></i>&nbsp;Add Holiday
-</button>
+  <div class="d-grid gap-2">
 
 
+  <form method="POST">
+  <div class="input-group input-group-sm mb-1">
+  
+  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Holidays">
+</div>
+ 
+<div class="input-group input-group-sm mb-1">
+ 
+  <input type="date" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+</div>
+ 
+  <button type="submit" class="btn btn-success btn-sm mb-2 w-100">Add Holiday</button>
+</form>
 
 
-<?php include("../Modals/M-Holiday.php")?>  
+
   </div>
 
 
@@ -42,8 +52,10 @@
 
 
 
+  
 
-<div class="overflow-auto w-100" style="height: 15rem;">
+
+<div class="overflow-auto w-100" style="height: 12rem;">
 
 
 <!--buong card to-->
@@ -69,6 +81,8 @@
       </div>
     </div>
   </div>
+
+  
 <!--buong card to-->
 
 
@@ -124,3 +138,6 @@
     
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+                            </body>
+                            </html>
