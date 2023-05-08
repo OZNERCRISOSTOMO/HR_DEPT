cbc69eaa63aad9181493ea55070a2f447dc3309a
John Renzo M. Crisostomo <johnrenzo.crisostomo07@gmail.com>
2023-05-08 21:39:03 +0800

    update

---
+++ Functions/leave-accept.php
@@ -0,0 +1,65 @@
+<?php
+
+    $dbServername = "sql985.main-hosting.eu";
+    $dbUsername = "u839345553_sbit3g";
+    $dbPassword = "sbit3gQCU";
+
+    $conn = new mysqli($dbServername, $dbUsername, $dbPassword, 'u839345553_SBIT3G');
+
+     if ($conn->connect_error) {
+         die("Connection failed: " . $conn->connect_error);
+     }
+     
+     if(isset($_POST['acceptbtn'])){
+        $id = $_POST['acceptid'];
+        $overtime1 = "SELECT * FROM `leave` WHERE id = $id";
+        $queryko = $conn->query($overtime1);
+        $date = $queryko->fetch_assoc();
+        $date_end = $date['date_ended'];
+        $date_start = $date['date_started'];
+
+        $diff_absent = "SELECT DATEDIFF('$date_end', '$date_start') AS days, sick_leave, vacation_leave FROM employee_details";
+        $diff_query = $conn->query($diff_absent);
+        $diff_row = $diff_query->fetch_assoc();
+
+        if($date['Type'] == "Sick Leave"){
+            $update_leave_days = "UPDATE employee_details SET sick_leave = sick_leave - '".$diff_row['days']."' WHERE employee_id = '$id'";
+            $conn->query($update_leave_days);
+            
+            $delete_absent = "DELETE FROM attendance WHERE (date BETWEEN '$date_start' AND '$date_end') AND employee_id = '$id'";
+            $delete_query = $conn->query($delete_absent);
+
+            $update_stat = "UPDATE `leave` SET status = 1 WHERE id = '$id'";
+            $conn->query($update_stat);
+        }else if($date['Type'] == "Vacation Leave"){
+            $start = strtotime($date_start);
+            $end = strtotime($date_end);
+            $interval = 86400;
+
+            $employeee = "SELECT * FROM employees WHERE id = '".$date['employee_id']."'";
+            $employee_query = $conn->query($employeee);
+            $employee_row = $employee_query->fetch_assoc();
+            $name = "".$employee_row['first_name']." ".$employee_row['last_name']."";
+            for ($timestamp = $start; $timestamp <= $end; $timestamp += $interval) {
+                $formatted_date = date('Y-m-d', $timestamp);
+
+                $holiday = "SELECT * FROM holiday WHERE holiday_date = '$formatted_date' AND percentage = 2";
+                $hqueryy = $conn->query($holiday);
+
+                if($hqueryy->num_rows > 0){
+                    $sahod = 8 * 2;
+                }else{
+                    $sahod = 8;
+                }
+                
+                $insertVac = "INSERT INTO attendance (employee_id, name, date, time_in, status, num_hr, schedule_id) VALUES ('".$date['employee_id']."', '$name', '$formatted_date', 'null', 'VACATION LEAVE', '$sahod', '".$employee_row['schedule_id']."')";
+                $conn->query($insertVac);
+
+                $updateSalary = "UPDATE employee_details SET num_hr = num_hr + $sahod WHERE employee_id = '".$date['employee_id']."'";
+                $conn->query($updateSalary);
+            }
+
+            $update_stat = "UPDATE `leave` SET status = 1 WHERE id = '$id'";
+            $conn->query($update_stat);
+        }
+     }
--- Modals/M-Holiday.php
+++ Modals/M-Holiday.php
@@ -20,7 +20,20 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        <?php 
+        // Assume $start_date and $end_date are the two input dates in 'YYYY-MM-DD' format
+$start = new DateTime('2023-02-01');
+$end = new DateTime('2023-02-15');
+$interval = new DateInterval('P1D'); // Interval of 1 day
+
+// Loop through each date between the start and end dates
+for ($date = $start; $date <= $end; $date->add($interval)) {
+    $formatted_date = $date->format('Y-m-d');
+    echo $formatted_date . "\n";
+    // Do something with the date, such as query a database
+}
+
+        ?>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
