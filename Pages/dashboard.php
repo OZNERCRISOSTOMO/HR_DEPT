e36936ca5c104c1079c3f9c44f1250bbe2552867
John Renzo M. Crisostomo <johnrenzo.crisostomo07@gmail.com>
2023-05-15 22:27:01 +0800

    upup

--- Pages/dashboard.php
+++ Pages/dashboard.php
@@ -531,37 +531,6 @@ else {
 </div>
 </div>
 
-<!-- <script>
-const urlParams=new URLSearchParams(window.location.search);
-const successValue1 = urlParams.get('value')
-
-if(successValue1 === "delete"){
-Swal.fire({
-    position: "top-end",
-    icon: "success",
-    title: "Deleted!",
-    showConfirmButton: false,
-    timer: 3000,
-   timerProgressBar: true,
-   toast: true,
-
-  });
-}
-
-if(successValue1 === "holiday"){
-Swal.fire({
-    position: "top-end",
-    icon: "success",
-    title: "Successfully Add holiday!",
-    showConfirmButton: false,
-    timer: 3000,
-   timerProgressBar: true,
-   toast: true,
-
-  });
-}
-                  </script> -->
-
  <script>
 
  // Get the dropdown menu element
